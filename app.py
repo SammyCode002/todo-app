43028afee638de974f96616940776f8ba27952d5
Samuel Dameg <samvista808@gmail.com>
2026-03-13 10:21:31 -1000

    Remove unused jsonify import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os
 import requests
